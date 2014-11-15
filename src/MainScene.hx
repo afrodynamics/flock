21bd777a7ed4efae29d308abf4f8dec76f9c34de
patrickgh3 <patrickjtraynor@gmail.com>
2014-11-14 20:36:57 -0800

    stuff

--- src/MainScene.hx
+++ src/MainScene.hx
@@ -1,9 +1,11 @@
 import com.haxepunk.Scene;
+import com.haxepunk.Entity;
+import com.haxepunk.graphics.Backdrop;
 
 class MainScene extends Scene
 {
 	public override function begin()
 	{
-
+		add(new Entity(0, 0, new Backdrop("graphics/tile.png")));
 	}
 }
