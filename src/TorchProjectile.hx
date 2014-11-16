5481cdc959ba58fa87417ab8dbbf9a79ce77b77d
patrickgh3 <patrickjtraynor@gmail.com>
2014-11-15 17:33:54 -0800

    tweak bullet spawn

--- src/TorchProjectile.hx
+++ src/TorchProjectile.hx
@@ -13,21 +13,20 @@ class TorchProjectile extends FlockEntity {
 	var velocity:Point;
 
 	public override function new(x:Float, y:Float) {
-		super(x,y);
+		super(x - 8, y - 8);
 		image = new Image("graphics/fire-projectile.png");
 		graphic = image;
 		width = image.width;
 		height = image.height;
-		x -= width / 2;
-		y -= height / 2;
+		setHitbox(12, 12, -2, -2);
 
 		// Calculate the direction to where this bullet needs to go
 		// MouseX/Y coordinates are in viewport coordinates, so we need to offset them by the
 		// camera position to ensure our vector is accurate no matter where this bullet starts
 		velocity = new Point( HXP.camera.x + Input.mouseX - MainScene.player.centerX, HXP.camera.y + Input.mouseY - MainScene.player.centerY );
-		velocity.normalize(moveSpeed * 10);
+		velocity.normalize(moveSpeed * 6);
 		type = "projectile";
-		//moveBy(velocity.x, velocity.y);
+		moveBy(velocity.x, velocity.y);
 		velocity.normalize(moveSpeed);
 		layer = -10;
 	}
