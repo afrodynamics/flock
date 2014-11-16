75c1ad9ae74d7be80f1875565d91922019c2629e
Dexter Friedman <drfriedm@ucsd.edu>
2014-11-15 17:29:05 -0800

    updated some sprites, added sprite to monster

--- src/Monster.hx
+++ src/Monster.hx
@@ -1,6 +1,7 @@
 package ;
 import com.haxepunk.Entity;
 import com.haxepunk.graphics.Image;
+import com.haxepunk.graphics.Spritemap;
 import com.haxepunk.HXP;
 import flash.geom.Point;
 
@@ -12,12 +13,17 @@ class Monster extends Entity
 	private var target:Sheep;
 	private var velocity:Point = new Point();
 	private var speed:Float = 1.5;
+	private var sprite:Spritemap;
 
 	public function new() 
 	{
 		super();
 		chooseRandomSheep();
-		graphic = Image.createRect(24, 24);
+		sprite = new Spritemap("graphics/eye-critter-flying.png", 24, 24 );
+		sprite.add("down", [0]);
+		sprite.add("side", [1]);
+		sprite.add("up", [2]);
+		graphic = sprite;
 		type = "monster";
 		setHitbox(16, 16, -4, -4);
 		
@@ -51,6 +57,14 @@ class Monster extends Entity
 		velocity.y = target.centerY - centerY;
 		velocity.normalize(speed);
 		moveBy(velocity.x, velocity.y);
+
+		// Choose which sprite to use
+		if ( velocity.y < 0 ) {
+			sprite.play("up");
+		}
+		else {
+			sprite.play("down");
+		}
 		
 		var collide:Entity = collideTypes("sheep", x, y);
 		if (collide != null && collide != MainScene.player)
